910dfe4558e2d726b07869ab2a16909dd146383f
Joeun Lee <Joeun@Admins-MacBook-Pro-4.local>
2018-06-21 11:48:21 +0200

    Inital commit

---
+++ script.js
@@ -0,0 +1,60 @@
+
+$(function(){
+  $(".card_1").draggable({
+    handle:  '.card_1',
+    }); 
+});
+
+$(function(){
+  $(".card_2").draggable({
+    handle:  '.card_2',
+    }); 
+});
+
+$(function(){
+  $(".card_3").draggable({
+    handle:  '.card_3',
+    }); 
+});
+
+$(function(){
+  $(".card_4").draggable({
+    handle:  '.card_4',
+    }); 
+});
+
+$(function(){
+  $(".card_5").draggable({
+    handle:  '.card_5',
+    }); 
+});
+
+$(function(){
+  $(".card_6").draggable({
+    handle:  '.card_6',
+    }); 
+});
+
+$(function(){
+  $(".card_7").draggable({
+    handle:  '.card_7',
+    }); 
+});
+
+$(function(){
+  $(".card_8").draggable({
+    handle:  '.card_8',
+    }); 
+});
+
+$(function(){
+  $(".card_9").draggable({
+    handle:  '.card_9',
+    }); 
+});
+
+$(function(){
+  $(".card_10").draggable({
+    handle:  '.card_10',
+    }); 
+});
